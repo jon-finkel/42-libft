c125d0bc841e28f96377ffa6a27e722507163a66
jon-finkel <jon@jonnimrod.com>
2017-12-21 22:48:23 +0100

    v0.2. Modified ft_setenv and ft_unsetenv to work on a dynamic allocated copy of environ instead of the original.

--- api_functions/ft_setenv.c
+++ api_functions/ft_setenv.c
@@ -6,77 +6,55 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/20 19:00:38 by nfinkel           #+#    #+#             */
-/*   Updated: 2017/12/21 15:45:04 by nfinkel          ###   ########.fr       */
+/*   Updated: 2017/12/21 22:47:20 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-static int			extend_and_rewrite(const char *value, char *ptr, size_t len,
-					int k)
+/*
+** Do not use on original environ array, make a copy of it and then, use it
+** on the copy. If used on original environ array, the behavior is undefined.
+*/
+
+static int			realloc_and_expand(const char *name, const char *value,
+					size_t len)
 {
+	char			**env_copy;
 	extern char		**environ;
-	int				p;
-	size_t			p_len;
+	int				k;
 
-	p = k;
-	while (environ[p + 1])
-		++p;
-	while (p != k)
-	{
-		p_len = ft_strlen(environ[p]);
-		ft_memmove(environ[p] + len, environ[p], p_len);
-		environ[p] += len;
-		environ[p][p_len] = '\0';
-		--p;
-	}
-	ft_strcpy(ptr, value);
-	return (0);
-}
-
-static int			rewrite_variable(const char *name, const char *value, int k)
-{
-	char			*ptr;
-	extern char		**environ;
-	ssize_t			len;
-
-	ptr = ft_strchr(environ[k], '=') + 1;
-	len = ft_strlen(environ[k]) - (ft_strlen(name) + ft_strlen(value) + 1);
-	if (!len)
-		ft_strcpy(ptr, value);
-	else if (len > 0)
-		ft_strcpy(ptr, value);
-	else
-		return (extend_and_rewrite(value, ptr, -len, k));
+	PROTECT(env_copy = (char **)malloc(sizeof(char *) * (len + 1)), -1);
+	k = -1;
+	while (environ[++k])
+		PROTECT(env_copy[k] = ft_strdup(environ[k]), -1);
+	NEG_PROTECT(ft_asprintf(&env_copy[k], "%s=%s", name, value), -1);
+	env_copy[++k] = NULL;
+	ft_cleanup(2, APTR, environ);
+	environ = env_copy;
 	return (0);
 }
 
 int					ft_setenv(const char *name, const char *value,
 					int overwrite)
 {
-	char			variable[512];
+	char			variable[4096];
 	extern char		**environ;
 	int				k;
-	int				p;
-	size_t			len;
 
-	if (!name || !*name || ft_strchr(name, '='))
-		return (-1);
 	k = -1;
 	while (environ[++k])
 	{
-		ft_memset(variable, '\0', 512);
-		ft_memnccpy(variable, environ[k], '=', 512);
-		if (ft_strequ(name, variable))
-			return ((overwrite ? rewrite_variable(name, value, k) : 0));
+		ft_memnccpy(variable, environ[k], '=', 4096);
+		if (ft_strequ(variable, name))
+		{
+			if (overwrite)
+			{
+				free(environ[k]);
+				NEG_PROTECT(ft_asprintf(&environ[k], "%s=%s", name, value), -1);
+			}
+			return (0);
+		}
 	}
-	ft_sprintf(variable, "%s=%s", name, value);
-	len = ft_strlen(environ[k - 1]);
-	environ[k] = environ[k - 1] + len + 1;
-	p = -1;
-	while (variable[++p])
-		environ[k - 1][++len] = variable[p];
-	environ[k - 1][++len] = '\0';
-	environ[k + 1] = NULL;
-	return (0);
+	return (realloc_and_expand(name, value, (size_t)(k + 1)));
 }
--- api_functions/ft_unsetenv.c
+++ api_functions/ft_unsetenv.c
@@ -6,33 +6,58 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/20 19:00:50 by nfinkel           #+#    #+#             */
-/*   Updated: 2017/12/21 15:41:32 by nfinkel          ###   ########.fr       */
+/*   Updated: 2017/12/21 22:47:31 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-int			ft_unsetenv(const char *name)
+/*
+** Do not use on original environ array, make a copy of it and then, use it
+** on the copy. If used on original environ array, the behavior is undefined.
+*/
+
+static int			copy_and_relocate(size_t len, int n)
 {
-	char			variable[512];
+	char			**env_copy;
+	extern char		**environ;
+	char			***aenv;
+	int				k;
+	int				p;
+
+	PROTECT(env_copy = (char **)malloc(sizeof(char *) * (len)), -1);
+	k = -1;
+	p = -1;
+	while ((unsigned int)++k < len - 1)
+	{
+		if (k == n)
+			++p;
+		PROTECT(env_copy[k] = ft_strdup(environ[++p]), -1);
+	}
+	env_copy[k] = NULL;
+	aenv = &environ;
+	ft_cleanup(2, APTR, environ);
+	*aenv = env_copy;
+	return (0);
+}
+
+int					ft_unsetenv(const char *name)
+{
+	char			variable[4096];
 	extern char		**environ;
 	int				k;
+	size_t			len;
 
-	if (!name || !*name || ft_strchr(name, '='))
-		return (-1);
 	k = -1;
 	while (environ[++k])
 	{
-		ft_memset(variable, '\0', 512);
-		ft_memnccpy(variable, environ[k], '=', 512);
-		if (ft_strequ(name, variable))
+		ft_memnccpy(variable, environ[k], '=', 4096);
+		if (ft_strequ(variable, name))
 		{
-			while (environ[k + 1])
-			{
-				environ[k] = environ[k + 1];
-				++k;
-			}
-			environ[k] = NULL;
+			len = k;
+			while (environ[len])
+				++len;
+			return (copy_and_relocate(len, k));
 		}
 	}
 	return (0);
