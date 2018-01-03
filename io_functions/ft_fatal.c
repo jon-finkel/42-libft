930525e576e621edb90d0a329f24f218539d5696
jon-finkel <jon@jonnimrod.com>
2018-01-03 19:37:12 +0000

    v0.2.4 Added newline to ft_fatal.

--- io_functions/ft_fatal.c
+++ io_functions/ft_fatal.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/31 16:46:11 by nfinkel           #+#    #+#             */
-/*   Updated: 2017/12/31 16:46:55 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/01/03 19:36:36 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void			ft_fatal(const char *s)
 {
 	if (s)
-		ft_dprintf(STDERR_FILENO, "fatal: %s", s);
+		ft_dprintf(STDERR_FILENO, "fatal: %s\n", s);
 	exit(EXIT_FAILURE);
 }
