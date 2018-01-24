ad5d07fa1b1de48d21cc37b13ae63c8eebbaac3e
jon-finkel <nfinkel@student.42.fr>
2018-01-24 18:53:45 +0100

    v0.3.0 Few modifications.

--- api_functions/ft_getenv.c
+++ api_functions/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/25 22:58:26 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/24 18:31:26 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/01/24 18:53:37 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ char			*ft_getenv(const char *name)
 	len = ft_strlen(name);
 	k = -1;
 	while (environ[++k])
-		if (ft_strnequ((char *)(environ[k]), name, len))
+		if (ft_strnequ((char *)(environ[k]), name, len)
+			&& environ[k][len] == '=')
 			GIMME(ft_strchr(environ[k], '=') + 1);
 	ZOMG;
 }
