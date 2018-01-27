04355f9423233f19d8363fbe88014c134af467b0
jon-finkel <nfinkel@student.42.fr>
2018-01-27 15:21:51 +0100

    v0.3.0 Modified GNL code.

--- io_functions/get_next_line.c
+++ io_functions/get_next_line.c
@@ -6,24 +6,26 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 15:16:11 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/27 15:01:14 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/01/27 15:21:39 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line_private.h"
 
-static char				*strnjoin(const char *s1, const char *s2, size_t len)
+static inline char		*strnjoin(const char *s1, const char *s2, size_t len)
 {
 	char	*s;
 	char	*ret;
+	char	*tmp;
 
 	s = ft_strnew(ft_strlen(s1) + len + 1);
 	ret = s;
+	tmp = (char *)s1;
 	while (*s1)
 		*s++ = *s1++;
 	while (*s2 && len--)
 		*s++ = *s2++;
-	free((char *)s1);
+	free(tmp);
 	GIMME(ret);
 }
 
@@ -62,12 +64,11 @@ int						get_next_line(int const fd, char **line)
 	while (((char*)list->content)[ret] && ((char*)list->content)[ret] != '\n')
 		++ret;
 	*line = ft_strndup(list->content, ret);
-	(((char*)list->content)[ret] == '\n') ? ++ret : 0;
+	if (((char*)list->content)[ret] == '\n')
+		++ret;
 	tmp = list->content;
 	list->content = ft_strdup(tmp + ret);
 	list = begin;
 	free(tmp);
-	if (ret)
-		GIMME(1);
-	KTHXBYE;
+	GIMME(ret ? 1 : 0);
 }
