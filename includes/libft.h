e23f028a765b135f0bd9d6636316ed02a54b5d9d
jon-finkel <jon@jonnimrod.com>
2018-02-03 17:32:43 +0000

    v0.3.1 Added dlist.h and math.h to libft.h.

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 17:50:38 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/24 18:31:12 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/03 17:32:24 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 # include "./dependencies.h"
 # include "../api_functions/api.h"
 # include "../character_functions/character.h"
+# include "../dlist_functions/dlist.h"
 # include "../io_functions/io.h"
 # include "../linkedlist_functions/linkedlist.h"
+# include "../math_functions/math.h"
 # include "../memory_functions/memory.h"
 # include "../string_functions/string.h"
 # include "../vector_functions/vector.h"
