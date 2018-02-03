bfdbfdc0cecc7ab127c993b117e6f9cede98f705
jon-finkel <jon@jonnimrod.com>
2018-02-03 16:53:41 +0000

    v0.3.1 Added malloc protection in get_next_line.

--- io_functions/get_next_line.c
+++ io_functions/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 15:16:11 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/03 14:25:33 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/03 16:53:12 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static inline char		*strnjoin(const char *s1, const char *s2, size_t len)
 	char	*ret;
 	char	*tmp;
 
-	s = ft_strnew(ft_strlen(s1) + len + 1);
+	FAILZ(s = ft_strnew(ft_strlen(s1) + len + 1), NULL);
 	ret = s;
 	tmp = (char *)s1;
 	while (*s1)
@@ -40,7 +40,7 @@ static t_list			*get_node(t_list **begin, int fd)
 			GIMME(list);
 		list = list->next;
 	}
-	list = ft_lstnew("\0", 1);
+	FAILZ(list = ft_lstnew("\0", 1), NULL);
 	list->data_size = fd;
 	ft_lstadd(begin, list);
 	GIMME(list);
@@ -57,17 +57,17 @@ int						get_next_line(int const fd, char **line)
 	if (fd < 0 || line == NULL || read(fd, buff, 0) < 0)
 		ONOES;
 	begin = list;
-	list = get_node(&begin, fd);
+	FAILZ(list = get_node(&begin, fd), -2);
 	while (!ft_strchr(list->data, '\n') && (ret = read(fd, buff, BUFF_SIZE)))
-		list->data = strnjoin(list->data, buff, ret);
+		FAILZ(list->data = strnjoin(list->data, buff, ret), -2);
 	ret = 0;
 	while (((char*)list->data)[ret] && ((char*)list->data)[ret] != '\n')
 		++ret;
-	*line = ft_strndup(list->data, ret);
+	FAILZ(*line = ft_strndup(list->data, ret), -2);
 	if (((char*)list->data)[ret] == '\n')
 		++ret;
 	tmp = list->data;
-	list->data = ft_strdup(tmp + ret);
+	FAILZ(list->data = ft_strdup(tmp + ret), -2);
 	list = begin;
 	free(tmp);
 	GIMME(ret ? 1 : 0);
