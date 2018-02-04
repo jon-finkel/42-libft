181ca026f750d2aa752008b6116caed464945d8c
jon-finkel <jon@jonnimrod.com>
2018-02-04 17:34:33 +0000

    v0.3.2 Renamed mlx_keys.h defines to prevent collision.

---
+++ includes/mlx_keys.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_keys.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/04 17:33:25 by nfinkel           #+#    #+#             */
+/*   Updated: 2018/02/04 17:34:13 by nfinkel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MLX_KEYS_H
+# define MLX_KEYS_H
+
+# define MLX_KEY_A 0
+# define MLX_KEY_S 1
+# define MLX_KEY_D 2
+# define MLX_KEY_F 3
+# define MLX_KEY_H 4
+# define MLX_KEY_G 5
+# define MLX_KEY_Z 6
+# define MLX_KEY_X 7
+# define MLX_KEY_C 8
+# define MLX_KEY_V 9
+# define MLX_KEY_B 11
+# define MLX_KEY_Q 12
+# define MLX_KEY_W 13
+# define MLX_KEY_E 14
+# define MLX_KEY_R 15
+# define MLX_KEY_Y 16
+# define MLX_KEY_T 17
+# define MLX_KEY_1 18
+# define MLX_KEY_2 19
+# define MLX_KEY_3 20
+# define MLX_KEY_4 21
+# define MLX_KEY_6 22
+# define MLX_KEY_5 23
+# define MLX_KEY_EQUAL 24
+# define MLX_KEY_9 25
+# define MLX_KEY_7 26
+# define MLX_KEY_MINUS 27
+# define MLX_KEY_8 28
+# define MLX_KEY_0 29
+# define MLX_KEY_O 31
+# define MLX_KEY_U 32
+# define MLX_KEY_I 34
+# define MLX_KEY_P 35
+# define MLX_KEY_ENTER 36
+# define MLX_KEY_L 37
+# define MLX_KEY_J 38
+# define MLX_KEY_K 40
+# define MLX_KEY_N 45
+# define MLX_KEY_M 46
+# define MLX_KEY_TAB 48
+# define MLX_KEY_SPACE 49
+# define MLX_KEY_BACKTICK 50
+# define MLX_KEY_BACKSPACE 51
+# define MLX_KEY_ESCAPE 53
+# define MLX_KEY_DEL 117
+# define MLX_KEY_L_ARROW 123
+# define MLX_KEY_R_ARROW 124
+# define MLX_KEY_U_ARROW 126
+# define MLX_KEY_D_ARROW 125
+# define MLX_KEY_L_CTRL 256
+# define MLX_KEY_L_SHIFT 257
+# define MLX_KEY_R_SHIFT 258
+# define MLX_KEY_L_ALT 259
+# define MLX_KEY_R_ALT 260
+# define MLX_KEY_R_CTRL 269
+
+#endif
