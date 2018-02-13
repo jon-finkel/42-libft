5e815dc1e5341dec4b58848b5c8cc031ca676ab6
jon-finkel <jon@jonnimrod.com>
2018-02-13 07:41:48 +0000

    v0.3.3 Added ft_wralloc and ft_errhdl. Removed protection defines. Removed inline keyword and segfault protection from some functions.

--- dlist_functions/ft_dlstnew.c
+++ dlist_functions/ft_dlstnew.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 14:23:29 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/12 20:17:38 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/13 07:35:14 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ t_dlist			*ft_dlstnew(const void *data, size_t data_size)
 	t_dlist		*dlist;
 
 	dlist = (t_dlist *)ft_memalloc(sizeof(t_dlist));
-	dlist->data = ft_wralloc(data_size);
-	dlist->data = ft_memmove(dlist->data, data, data_size);
-	dlist->data_size = data_size;
+	if (data)
+	{
+		dlist->data = ft_wralloc(data_size);
+		dlist->data = ft_memmove(dlist->data, data, data_size);
+		dlist->data_size = data_size;
+	}
 	GIMME(dlist);
 }
--- linkedlist_functions/ft_lstnew.c
+++ linkedlist_functions/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/24 21:30:33 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/12 20:21:27 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/13 07:34:15 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,11 @@ t_list			*ft_lstnew(const void *data, size_t data_size)
 	t_list		*list;
 
 	list = (t_list *)ft_memalloc(sizeof(t_list));
-	list->data = ft_wralloc(data_size);
-	list->data = ft_memmove(list->data, data, data_size);
-	list->data_size = data_size;
+	if (data)
+	{
+		list->data = ft_wralloc(data_size);
+		list->data = ft_memmove(list->data, data, data_size);
+		list->data_size = data_size;
+	}
 	GIMME(list);
 }
