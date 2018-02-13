9c851a4226c41882826278ba8215b7f595bf5439
jon-finkel <jon@jonnimrod.com>
2018-02-13 07:28:30 +0000

    v0.3.3 Added ft_wralloc and ft_errhdl. Removed protection defines. Removed inline keyword and segfault protection from some functions.

--- string_functions/ft_strncmp.c
+++ string_functions/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/24 10:54:41 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/12 20:27:40 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/13 07:26:28 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int			ft_strncmp(const char *restrict s1, const char *restrict s2,
 			size_t n)
 {
-	while (n && --n && *s1 && *(unsigned char *)s1 == *(unsigned char *)s2)
+	if (!n)
+		KTHXBYE;
+	while (--n && *s1 && *(unsigned char *)s1 == *(unsigned char *)s2)
 	{
 		++s1;
 		++s2;
 	}
-	GIMME(!n || *(unsigned char *)s1 - *(unsigned char *)s2);
+	GIMME(*(unsigned char *)s1 - *(unsigned char *)s2);
 }
--- string_functions/ft_strnequ.c
+++ string_functions/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/24 18:41:21 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/12 20:28:42 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/13 07:27:19 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int			ft_strnequ(const char *restrict s1, const char *restrict s2,
 			size_t n)
 {
-	while (n && --n && *s1 && *(unsigned char *)s1 == *(unsigned char *)s2)
+	if (!n)
+		GIMME(1);
+	while (--n && *s1 && *(unsigned char *)s1 == *(unsigned char *)s2)
 	{
 		++s1;
 		++s2;
 	}
-	GIMME(!n || *s1 == *s2 ? 1 : 0);
+	GIMME(*s1 == *s2 ? 1 : 0);
 }
