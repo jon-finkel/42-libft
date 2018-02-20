0fead4a0333159e7781139ca95f28dce5384f99a
jon-finkel <jon@jonnimrod.com>
2018-02-20 18:36:09 +0000

    v0.3.4 Added _Noreturn keyword to ft_fatal.

--- io_functions/ft_fatal.c
+++ io_functions/ft_fatal.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/31 16:46:11 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/02/20 18:29:58 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/20 18:36:00 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 _Noreturn void			ft_fatal(const char *restrict s)
 {
-	ft_dprintf(STDERR_FILENO, "fatal: %s\n", s);
+	ft_putstr_fd("fatal: ", STDERR_FILENO);
+	ft_putendl_fd(s, STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
