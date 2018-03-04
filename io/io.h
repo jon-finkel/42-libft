20fbbab87cac9dcd9eeaf9cf6934738f25a0be5c
jon-finkel <nfinkel@student.42.fr>
2018-03-04 17:50:09 +0100

    v0.3.5 Added ft_exit.

--- io/io.h
+++ io/io.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 18:39:27 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/04 17:39:00 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/04 17:49:57 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,6 @@ extern int			ft_vprintf(const char *format, va_list ap);
 extern int			ft_vsprintf(char *str, const char *format, va_list ap);
 extern int			ft_vsnprintf(char *str, size_t size, const char *format,
 					va_list ap);
-extern int			ft_wread(int filedes, void *buf, size_t nbyte);
-extern int			ft_wropen(const char *path, int oflag, ...);
 extern int			get_next_line(const int fd, char **line);
 
 _Noreturn void		ft_exit(const char *s, const int ret);
