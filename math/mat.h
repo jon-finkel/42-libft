d0be506f29b3886e86ba1c4ab98215bad1b311e4
jon-finkel <jon@jonnimrod.com>
2018-03-11 22:26:16 +0100

    v0.3.5 Removed ft_polynomial.

--- math/mat.h
+++ math/mat.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 18:39:46 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/11 20:31:40 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/11 22:25:29 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 
 extern size_t			ft_nextpower2(size_t nb);
 extern int				ft_pgcd(int a, int b);
-extern void				ft_polynomial(const double *p, const int degree);
 
 #endif
