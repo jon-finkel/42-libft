0a6cf4753543fd4ef91884085bd6c9d2cd73ce5b
jon-finkel <jon@jonnimrod.com>
2018-03-19 15:53:30 +0100

    v0.3.6 Added ft_mlxinit, ft_mlxaddwin and ft_vecnew.

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 17:50:38 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/14 10:30:17 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/19 15:53:28 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # include "libft/list.h"
 # include "libft/mat.h"
 # include "libft/mem.h"
+# include "libft/mlx.h"
 # include "libft/str.h"
 # include "libft/vary.h"
 
