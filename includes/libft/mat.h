eda4c0634fd4f5d7aa169cd7174a9401332f63b3
jon-finkel <nfinkel@student.42.fr>
2018-03-22 16:28:26 +0100

    v0.3.6 Added ft_vecrotx, ft_vecroty and ft_vecrotz.

--- includes/libft/mat.h
+++ includes/libft/mat.h
@@ -6,29 +6,30 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 18:39:46 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/19 15:32:08 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/22 16:27:21 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAT_H
 # define MAT_H
 
+# include <math.h>
 # include "dependencies.h"
 
 typedef struct		s_vec4
 {
-	float			x;
-	float			y;
-	float			z;
+	double			x;
+	double			y;
+	double			z;
 	bool			w;
 }					t_vec4;
 
 typedef struct		s_matrix4
 {
-	float			a[4];
-	float			b[4];
-	float			c[4];
-	float			d[4];
+	double			a[4];
+	double			b[4];
+	double			c[4];
+	double			d[4];
 }					t_matrix4;
 
 extern size_t			ft_nextpower2(size_t nb);
@@ -36,6 +37,9 @@ extern int				ft_pgcd(int a, int b);
 extern int32_t			ft_32npow(const intmax_t nb, uint8_t power);
 extern int64_t			ft_64npow(const intmax_t nb, uint8_t power);
 extern size_t			ft_stnpow(const intmax_t nb, uint8_t power);
-extern t_vec4			*ft_vecnew(float x, float y, float z, bool w);
+extern t_vec4			*ft_vecnew(double x, double y, double z, bool w);
+extern t_vec4			*ft_vecrotx(t_vec4 *vec, double theta);
+extern t_vec4			*ft_vecroty(t_vec4 *vec, double theta);
+extern t_vec4			*ft_vecrotz(t_vec4 *vec, double theta);
 
 #endif
--- math/vec.c
+++ math/vec.c
@@ -6,14 +6,14 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 15:29:04 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/19 15:30:30 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/22 16:26:39 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/mat.h"
 #include "libft/mem.h"
 
-t_vec4			*ft_vecnew(float x, float y, float z, bool w)
+t_vec4			*ft_vecnew(double x, double y, double z, bool w)
 {
 	t_vec4		*vec;
 
@@ -22,5 +22,35 @@ t_vec4			*ft_vecnew(float x, float y, float z, bool w)
 	vec->y = y;
 	vec->z = z;
 	vec->w = w;
+	GIMME(vec);
+}
+
+t_vec4			*ft_vecrotx(t_vec4 *vec, double theta)
+{
+	double		tmp;
+
+	tmp = (sin(theta) * vec->y) + (cos(theta) * vec->z);
+	vec->y = (cos(theta) * vec->y) - (sin(theta) * vec->z);
+	vec->z = tmp;
+	GIMME(vec);
+}
+
+t_vec4			*ft_vecroty(t_vec4 *vec, double theta)
+{
+	double		tmp;
+
+	tmp = -(sin(theta) * vec->x) + (cos(theta) * vec->z);
+	vec->x = (cos(theta) * vec->x) + (sin(theta) * vec->z);
+	vec->z = tmp;
+	GIMME(vec);
+}
+
+t_vec4			*ft_vecrotz(t_vec4 *vec, double theta)
+{
+	double		tmp;
+
+	tmp = (sin(theta) * vec->x) + (cos(theta) * vec->y);
+	vec->x = (cos(theta) * vec->x) - (sin(theta) * vec->y);
+	vec->y = tmp;
 	GIMME(vec);
 }
