a524b245fa691f79ed6750bc2197c51584587447
jon-finkel <jon@jonnimrod.com>
2018-03-31 12:36:57 +0200

    Added ftx_setwin and ftx_setimg.

--- includes/libft/mlx.h
+++ includes/libft/mlx.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/04 17:33:25 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/31 12:15:40 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/31 12:36:36 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ typedef struct			s_mlx
 {
 	t_mlx_img			**img;
 	uint16_t			cur_img;
+	uint16_t			cur_win;
 	void				*mlx;
 	void				**win;
 }						t_mlx;
@@ -40,12 +41,14 @@ extern t_vary			*g_mlx_win_vary;
 
 extern void				ftx_addwin(t_mlx *mlx, int size_x, int size_y,
 						char *title);
-extern void				ftx_addimg(t_mlx *mlx, int width, int height);
+extern t_mlx_img		*ftx_addimg(t_mlx *mlx, int width, int height);
 extern t_mlx_img		*ftx_buffpixel(t_mlx_img *img, const int x, const int y,
 						int color);
 extern t_mlx_img		*ftx_hline(t_mlx_img *img, const t_vec4 v1,
 						const t_vec4 v2, int color);
 extern void				ftx_init(t_mlx *mlx);
+extern t_mlx_img		*ftx_setimg(t_mlx *mlx, uint16_t n);
+extern void				*ftx_setwin(t_mlx *mlx, uint16_t n);
 extern t_mlx_img		*ftx_vline(t_mlx_img *img, const t_vec4 v1,
 						const t_vec4 v2, int color);
 
--- mlx/init1.c
+++ mlx/init1.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 15:04:45 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/03/29 12:38:03 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/03/31 12:36:14 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ static t_vary		g_mlx_win_vary_null = {NULL, 0, 0, sizeof(void *)};
 t_vary				*g_mlx_img_vary = &g_mlx_img_vary_null;
 t_vary				*g_mlx_win_vary = &g_mlx_win_vary_null;
 
-void			ftx_init(t_mlx *mlx)
+void				ftx_init(t_mlx *mlx)
 {
 	ft_memset(mlx, '\0', sizeof(t_mlx));
 	if (!(mlx->mlx = mlx_init()))
 		ft_errhdl(NULL, 0, ERR_MLX);
 }
 
-void			ftx_addwin(t_mlx *mlx, int size_x, int size_y, char *title)
+void				ftx_addwin(t_mlx *mlx, int size_x, int size_y, char *title)
 {
 	void		*win;
 
@@ -36,7 +36,13 @@ void			ftx_addwin(t_mlx *mlx, int size_x, int size_y, char *title)
 	mlx->win = g_mlx_win_vary->buff;
 }
 
-void			ftx_addimg(t_mlx *mlx, int width, int height)
+void				*ftx_setwin(t_mlx *mlx, uint16_t n)
+{
+	mlx->cur_win = n;
+	GIMME(mlx->win[mlx->cur_win]);
+}
+
+t_mlx_img			*ftx_addimg(t_mlx *mlx, int width, int height)
 {
 	t_mlx_img		*img;
 
@@ -48,4 +54,11 @@ void			ftx_addimg(t_mlx *mlx, int width, int height)
 	img->addr = mlx_get_data_addr(img->img, &img->bppx, &img->sl, &img->endian);
 	*(t_mlx_img **)ft_varypush(g_mlx_img_vary) = img;
 	mlx->img = g_mlx_img_vary->buff;
+	GIMME(img);
+}
+
+t_mlx_img			*ftx_setimg(t_mlx *mlx, uint16_t n)
+{
+	mlx->cur_img = n;
+	GIMME(mlx->img[mlx->cur_img]);
 }
