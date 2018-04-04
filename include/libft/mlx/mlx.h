77825dc7ac11e6cd1965bab3d48124ea1bd81462
jon-finkel <nfinkel@student.42.fr>
2018-04-04 19:49:16 +0200

    Added ftx_blurrimg, and some mlx ctor and dtor functions.

--- include/libft/mlx/mlx.h
+++ include/libft/mlx/mlx.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/04 17:33:25 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/04/03 11:36:19 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/04/04 19:47:52 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,7 @@ typedef struct			s_mlx
 	void				**win;
 }						t_mlx;
 
-extern t_vary			*g_mlx_img_vary;
-extern t_vary			*g_mlx_win_vary;
-
-extern void				ftx_addwin(t_mlx *mlx, int size_x, int size_y,
-						char *title);
-extern t_mlx_img		*ftx_addimg(t_mlx *mlx, int width, int height);
+extern t_mlx_img		*ftx_blurrimg(t_mlx_img *img);
 extern t_mlx_img		*ftx_buffpixel(t_mlx_img *img, const int x, const int y,
 						int color);
 extern t_mlx_img		*ftx_clearimg(t_mlx_img *img);
@@ -55,15 +50,23 @@ extern t_mlx_img		*ftx_drawline(t_mlx_img *img, const t_vec4 v1,
 						const t_vec4 v2, int color);
 extern t_mlx_img		*ftx_hline(t_mlx_img *img, const t_vec4 v1,
 						const t_vec4 v2, int color);
+extern t_mlx_img		*ftx_imgctor(t_mlx *mlx, int width, int height);
+extern void				ftx_imgdtor(void *data, va_list ap);
 extern void				ftx_init(t_mlx *mlx);
+extern void				ftx_mlxdtor(t_mlx *mlx);
 extern t_mlx_img		*ftx_setimg(t_mlx *mlx, uint16_t n);
 extern void				*ftx_setwin(t_mlx *mlx, uint16_t n);
 extern t_mlx_img		*ftx_vline(t_mlx_img *img, const t_vec4 v1,
 						const t_vec4 v2, int color);
+extern void				ftx_winctor(t_mlx *mlx, int size_x, int size_y,
+						char *title);
+extern void				ftx_windtor(void *data, va_list ap);
 
 /*
 ** MLX functions
 */
+extern void				mlx_destroy_image(void *mlx_ptr, void *img_ptr);
+extern void				mlx_destroy_window(void *mlx_ptr, void *win_ptr);
 extern char				*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
 						int *size_line, int *endian);
 extern void				*mlx_new_image(void *mlx_ptr, int width, int height);
--- mlx/init1.c
+++ mlx/init1.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/19 15:04:45 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/04/03 11:37:30 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/04/04 19:48:23 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 static t_vary		g_mlx_img_vary_null = {NULL, 0, 0, sizeof(t_mlx_img *)};
 static t_vary		g_mlx_win_vary_null = {NULL, 0, 0, sizeof(void *)};
-t_vary				*g_mlx_img_vary = &g_mlx_img_vary_null;
-t_vary				*g_mlx_win_vary = &g_mlx_win_vary_null;
+static t_vary		*g_mlx_img_vary = &g_mlx_img_vary_null;
+static t_vary		*g_mlx_win_vary = &g_mlx_win_vary_null;
 
 void				ftx_init(t_mlx *mlx)
 {
@@ -26,7 +26,33 @@ void				ftx_init(t_mlx *mlx)
 		ft_errhdl(NULL, 0, ERR_MLX);
 }
 
-void				ftx_addwin(t_mlx *mlx, int size_x, int size_y, char *title)
+void				ftx_imgdtor(void *data, va_list ap)
+{
+	t_mlx		*mlx;
+
+	mlx = va_arg(ap, t_mlx *);
+	mlx_destroy_image(mlx->mlx, (*(t_mlx_img **)(data))->img);
+	ft_memdel((void **)&*(t_mlx_img **)data);
+}
+
+void				ftx_windtor(void *data, va_list ap)
+{
+	t_mlx		*mlx;
+
+	mlx = va_arg(ap, t_mlx *);
+	mlx_destroy_window(mlx->mlx, *(void **)(data));
+	ft_memdel((void **)data);
+}
+
+void				ftx_mlxdtor(t_mlx *mlx)
+{
+	ft_varydel(&g_mlx_img_vary, ftx_imgdtor, mlx);
+	ft_varydel(&g_mlx_win_vary, ftx_windtor, mlx);
+	ft_memdel((void **)&mlx->mlx);
+	ft_memdel((void **)&mlx);
+}
+
+void				ftx_winctor(t_mlx *mlx, int size_x, int size_y, char *title)
 {
 	void		*win;
 
@@ -42,7 +68,7 @@ void				*ftx_setwin(t_mlx *mlx, uint16_t n)
 	GIMME(_MLX_WIN_ID);
 }
 
-t_mlx_img			*ftx_addimg(t_mlx *mlx, int width, int height)
+t_mlx_img			*ftx_imgctor(t_mlx *mlx, int width, int height)
 {
 	t_mlx_img		*img;
 
@@ -54,6 +80,18 @@ t_mlx_img			*ftx_addimg(t_mlx *mlx, int width, int height)
 	img->addr = mlx_get_data_addr(img->img, &img->bppx, &img->sl, &img->endian);
 	*(t_mlx_img **)ft_varypush(g_mlx_img_vary) = img;
 	mlx->img = g_mlx_img_vary->buff;
+	GIMME(img);
+}
+
+t_mlx_img			*ftx_blurrimg(t_mlx_img *img)
+{
+	int		x;
+	int		y;
+
+	y = -1;
+	while (++y < img->height && (x = -1))
+		while (++x < img->width)
+			ftx_buffpixel(img, x, y, 0xA0000000);
 	GIMME(img);
 }
 
