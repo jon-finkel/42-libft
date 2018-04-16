9380db30d0fa945a9207539979262e0248b4a7db
jon-finkel <jon@jonnimrod.com>
2018-04-16 14:32:03 +0200

    Bug fix ft_zpowr && ft_zpowi.

--- math/pow2.c
+++ math/pow2.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 15:54:36 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/04/16 12:22:11 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/04/16 14:31:56 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ double			ft_zpowi(t_complex z, uint8_t n)
 		tmp = zp.r;
 		if (n > 1)
 			zp.r = z.r * zp.r - z.i * zp.i;
-		zp.i = z.r * zp.i + tmp * zp.i;
+		zp.i = z.r * zp.i + tmp * z.i;
 	}
 	GIMME(zp.i);
 }
@@ -64,7 +64,7 @@ double			ft_zpowr(t_complex z, uint8_t n)
 		tmp = zp.r;
 		zp.r = z.r * zp.r - z.i * zp.i;
 		if (n > 1)
-			zp.i = z.r * zp.i + tmp * zp.i;
+			zp.i = z.r * zp.i + tmp * z.i;
 	}
 	GIMME(zp.r);
 }
