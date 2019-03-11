7377d8ce7a8ea9f2d213d40e9b8d2ba794ff7552
Terry FINKEL <nfinkel@student.42.fr>
2019-03-11 22:39:23 +0100

    Add opts in header

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 17:50:38 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/04/22 15:30:04 by nfinkel          ###   ########.fr       */
+/*   Updated: 2019/03/11 22:39:18 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include "libft/mat.h"
 # include "libft/mem.h"
 # include "libft/mlxh.h"
+# include "libft/opt.h"
 # include "libft/str.h"
 # include "libft/vary.h"
 # include "libft/vec.h"
