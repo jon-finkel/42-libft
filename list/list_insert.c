f8850f727617090399b024877dbc7f4290a4aa47
terry-finkel <nfinkel@student.42.fr>
2019-03-19 20:30:57 +0100

    Remove useless lines

--- list/list_insert.c
+++ list/list_insert.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   insert.c                                           :+:      :+:    :+:   */
+/*   list_insert.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/25 21:45:18 by nfinkel           #+#    #+#             */
-/*   Updated: 2019/03/11 21:42:59 by nfinkel          ###   ########.fr       */
+/*   Updated: 2019/03/19 20:30:53 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ inline void	ft_lstinsert(t_list **alst, t_list *newlink,
 	list = *alst;
 	if (!list || cmp(list->data, newlink->data))
 		ft_lstadd(alst, newlink);
-	else if (!list->next)
-		list->next = newlink;
 	else
 	{
 		while (list->next)
